75dd254e787bf973aacd34547144b9562718b379
ChuSong327 <chuchusong23@gmail.com>
2019-01-02 13:57:29 -0800

    Refactor server.js to implement database connection once it's initiated. Build a route to retrieve a specific account's case number.

--- backend/server.js
+++ backend/server.js
@@ -6,26 +6,31 @@ const snowFlake = require("./db/index");
 const coreSalesTable = require('./config/table');
 
 const app = express();
+
+//CORS setting 
 const corsOptions = {
     origin: "http:localhost:8080",
     optionSuccessStatus: 200
 };
 
+//Use Middleware
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cookieParser());
 
+//Connect to SnowFlake database
+snowFlake.connect((err, conn) => {
+    if(err) {
+        console.log("This is the err:", err);
+    } else {
+        console.log("Successfully connected as id:", snowFlake.getId());
+    }
+});
 
+//Routes
 app.options("/singlepiedata", cors(corsOptions));
 app.get("/singlepiedata", cors(corsOptions), (req,res) => {
-    snowFlake.connect((err, conn) => {
-        if(err) {
-            console.log("This is the err:", err);
-        } else {
-            console.log("Successfully connected as id:", snowFlake.getId());
-        }
-    });
     snowFlake.execute({
         sqlText: `select billingcountry, count(1) as count from ${coreSalesTable} group by 1 order by 2 desc limit 20`,
         complete: (err, stmt, rows) => {
@@ -45,13 +50,6 @@ app.get("/singlepiedata", cors(corsOptions), (req,res) => {
 
 app.options("/account-names", cors(corsOptions));
 app.get("/account-names", cors(corsOptions), (req, res) => {
-    snowFlake.connect((err, conn) => {
-        if(err) {
-            console.log("This is the err:", err);
-        } else {
-            console.log("Successfully connected as id:", snowFlake.getId());
-        }
-    });
     snowFlake.execute({
         sqlText: `select distinct acct.name as account_name from ${coreSalesTable} as acct limit 100`,
         complete: (err, stmt, rows) => {
@@ -69,6 +67,40 @@ app.get("/account-names", cors(corsOptions), (req, res) => {
     })
 })
 
+app.get("/:acctName", (req, res) => {
+    const accountName = req.params.acctName;
+    snowFlake.execute({
+        sqlText: `select acct.name as account_name
+                        ,cs.severity__c as case_severity
+                        ,count(distinct cs.casenumber) as case_count
+                    from 
+                        core_sales_state.vw_sf_account_latest as acct
+                    inner join
+                        core_sales_state.vw_sf_case_latest as cs
+                        on acct.id = cs.accountid
+                    where
+                        acct.name like '${ accountName }'
+                    group by
+                        acct.name
+                        ,cs.severity__c
+                    order by 3 desc
+                    limit 1000;`,
+        complete: (err, stmt, rows) => {
+            if(err) {
+                console.log("This is the error message:", err.message);
+            } else {
+                console.log("Successfully executed statement:", stmt.getSqlText());
+                res.set({
+                    "Access-Control-Allow-Origin": "https://localhost:8080",
+                    "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE "
+                });
+                res.json(rows);
+            }
+        }
+    })
+})
+
+//Handle Errors
 app.use((req, res, next) => {
     next(createError(404));
 });
